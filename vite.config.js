fe8ca11f74a22947c90a0ed47b6bf4f72beabd83
Afridp <122078144+Afridp@users.noreply.github.com>
2024-04-30 18:16:34 +0530

    changed host

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'localhost',
+    host: '35.171.16.37',
     port: 3000,
   },
   optimizeDeps: {
