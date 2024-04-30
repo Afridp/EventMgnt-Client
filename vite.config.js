f8994e7e274df5e3d4a57b64925bee3abef0ae0c
Afridp <122078144+Afridp@users.noreply.github.com>
2024-04-30 11:15:07 +0530

    updated all modules and added build to config

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:'localhost',
-    port:3000
+  server: {
+    host: 'localhost',
+    port: 3000
   },
   optimizeDeps: {
     include: [
       '@reduxjs/toolkit',
       // other dependencies...
     ],
+  },
+  build: {
+    rollupOptions: {
+      external: ['@emotion/react/jsx-runtime'],
+    },
   },
 })
 
