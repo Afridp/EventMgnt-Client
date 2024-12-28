eae30815842dbde0eb4a92f10e7f5cabbf25cf45
Afridp <122078144+Afridp@users.noreply.github.com>
2024-12-28 22:06:06 +0530

    changed the function type

--- src/Pages/Tenents/Dashboard.jsx
+++ src/Pages/Tenents/Dashboard.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { completeSubscription } from "../../Api/manager";
 
-function Dashboard() {
+const Dashboard = () => {
   const ENV = import.meta.env.VITE_APP_NODE_ENV;
   console.log(ENV, "this is my env");
   const [open, setOpen] = useState(false);
